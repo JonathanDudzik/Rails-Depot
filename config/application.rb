00206637e04b5835439256d0685c861a2018ac56
JonathanWD <40643875+JonathanDudzik@users.noreply.github.com>
2021-10-05 16:01:59 -0400

    Setup postgres (#2)
    The default application was setup to use SQLite3 and we needed to use postgresql. We had to add postgres configuration to database.yml and add the pg gem to Gemfile. We also generated a basic table called product and the related model, view, and controller.

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,9 @@ module RailsDepot
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+ 
+    config.time_zone = "Central Time (US & Canada)"
+    config.beginning_of_week = :monday
   end
 end
